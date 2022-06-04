e15e04f4c048878043d985010e930a7d128a4d76
scortarudaniel <scortarudaniel@yahoo.com>
2022-06-04 11:18:41 +0300

    Add some Cart view modifications

--- Rocky_Models/ApplicationUser.cs
+++ Rocky_Models/ApplicationUser.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Rocky_Models
 {
     public class ApplicationUser : IdentityUser
     {
+
         public string FullName { get; set; }
+        [NotMapped]
+        [Display(Name = "Street address")]
+        public string StreetAddress { get; set; }
+        [NotMapped]
+        public string City { get; set; }
+        [NotMapped]
+        public string State { get; set; }
+        [NotMapped]
+        [Display(Name ="Postal code")]
+        public string PostalCode { get; set; }
     }
 }
